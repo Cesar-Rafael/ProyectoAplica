e646901b25f3b20495def57685df0b07783d40a6
Cesar-Rafael <crafael@pucp.edu.pe>
2021-06-07 13:25:14 -0500

    Changes by Manuel Loaiza

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 flex -o grupo5_ER.c grupo5_ER.l
 gcc grupo5_ER.c -o grupo5_ER -lfl -lm
-./grupo5_ER < tests/palabras_reservadas.raa > outputs/palabras_reservadas_resultado.out
-./grupo5_ER < tests/simbolos.raa > outputs/simbolos_resultado.out
+./grupo5_ER < path/to/tests/name.raa > path/to/outputs/name.out
